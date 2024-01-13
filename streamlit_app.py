a09616c56c325274ff9fa87450b7cf069d6932ea
Rachanakr <156527993+Rachanakr@users.noreply.github.com>
2024-01-13 21:21:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
